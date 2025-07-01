bd1dc1f68f288b47bbb66f852bb113b0b935bca2
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2025-07-01 00:11:07 -0700

    Refactor MagicBlock to use item tags for transformation
    Replaced hardcoded item check in MagicBlock with a tag-based approach using a new ModTags utility class. Added a transformable_items tag to allow flexible configuration of items that can be transformed by MagicBlock.

--- src/main/java/com/omgisa/examplemod/block/custom/MagicBlock.java
+++ src/main/java/com/omgisa/examplemod/block/custom/MagicBlock.java
@@ -1,6 +1,6 @@
 package com.omgisa.examplemod.block.custom;
 
-import com.omgisa.examplemod.item.ModItems;
+import com.omgisa.examplemod.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -31,7 +31,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull Entity entity) {
         if (entity instanceof ItemEntity itemEntity) {
-            if (itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()) {
+            if (isValidItem(itemEntity.getItem())) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
         }
@@ -42,5 +42,9 @@ public class MagicBlock extends Block {
             }
         }
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 }
